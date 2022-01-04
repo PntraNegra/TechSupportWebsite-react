0ad8ddcbe469a8070cfaff9ccc1eb3bc2faeef29
briancervantes <mr.cervantesgaona@gmail.com>
2022-01-04 15:31:47 -0800

    reactstrap form

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,7 +6,7 @@ function Footer(props) {
         <footer className="site-footer">
             <div className="container">
                 <div className="row">             
-                    <div className="col-4 col-sm-2 offset-1">
+                    <div className="col offset-sm-1">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li><Link to='/home'>Home</Link></li>
@@ -15,7 +15,8 @@ function Footer(props) {
                             <li><Link to='/learn'>Learn Something New</Link></li>
                         </ul>
                     </div>
-                    <div className="col-sm-4 text-center">
+                    <div className="col  offset-sm-1">
+                        <h5>Contact</h5>
                         <a role="button" className="btn btn-link" href="tel:+15303190982"><i className="fa fa-phone" /> 1(530)319-0982</a><br />
                         <a role="button" className="btn btn-link" href="mailto:bcervantes@wecarealot.org"><i className="fa fa-envelope-o" /> bcervantes@wecarealot.org</a>
                     </div>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,91 +1,144 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 
-function Home(props) {
-    return (
-        <div className="container mb-5">
-            <div className="row row-content">
-                <div className="col-12">
-                    <h2 className="text-center">We Care A Lot Tech Support</h2>
-                </div>
-            </div>
-            <div className="row row-content">
-                <div className="col-8">
-                    <h5>Welcome to the tech support home page!</h5><br />
-                    <p> For a request for technical support, I invite you to fill out the form below. Expect a response within 24 hours during business hours (Monday through Friday 8am to 5pm) otherwise feel free to continue throughout the website! </p><br />
-                    <h5>Think You Can Solve it Yourself?</h5>< br/>
-                    <h5 className="offset-2">Check Out Our Tutorials!</h5>
-                </div>
-                <div className="col"></div>
-            </div>
-            <div className="row row-content m-1">
-                <div className="col-12">
-                    <h2 className="text-center">Support Request</h2>
-                    <hr />
-                </div>
-                
-            </div>
-            <div className="col-md-10">
-                <form>
-                    <div className="form-group row">
-                        <label for="firstName" className="col-md-3 col-form-label">First Name(required)</label>
-                        <div className="col-md-9">
-                            <input type="text" className="form-control" name="firstName" id="firstName" placeholder="First Name" />
-                        </div> 
-                    </div>
-                    <div className="form-group row">
-                        <label for="lastName" className="col-md-3 col-form-label">Last Name(required)</label>
-                        <div className="col-md-9">
-                            <input type="text" className="form-control" id="lastName" name="lastName" placeholder="Last Name" />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label for="areaCode" className="col-md-3 col-form-label">Phone Number(optional)</label>
-                        <div className="col-5 col-md-3">
-                            <input type="tel" className="form-control" id="areaCode" name="areaCode" placeholder="Area Code" />
-                        </div>
-                        <div className="col-6">
-                            <input type="tel" className="form-control" name="telNum" placeholder="Tel. Number" />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label for="email" className="col-md-3 col-form-label">Email(required)</label>
-                        <div className="col-md-9">
-                            <input type="email" className="form-control" id="email" name="email" placeholder="Email" />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col">
-                            <label>Preferred form of response</label>{' '}
-                            <div className="form-check form-check-inline">
-                                <input type="radio" className="form-check-input" id="phoneCall" name="contactRadios" value="phoneCall" /> 
-                                <label for="phoneCall" className="form-check-label">Phone Call</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <input type="radio" className="form-check-input" id="textMessage" name="contactRadios" value="textMessage" /> 
-                                <label for="textMessage" className="form-check-label">Text Message</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <input type="radio" className="form-check-input" id="email" name="contactRadios" value="email" /> 
-                                <label for="email" className="form-check-label">Email</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label for="feedback" className="col-12 col-form-label">Please Describe Your Issue</label>
-                        <div className="col-12">
-                            <textarea placeholder="Click here to begin typing..." nameName="feedback" class="form-control" id="feedback" rows="8"></textarea>
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-12">
-                            <button type="submit" className="btn btn-lg btn-primary">Submit Request</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
+class Home extends Component {
 
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: '',
+            agree: false,
+            contactType: 'By Phone',
+            feedback: ''
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+    
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('Current state is: ' + JSON.stringify(this.state));
+        alert('Current state is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+
+    render() {
+        return (
+            <div className="container mb-5">
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h2 className="text-center">We Care A Lot Tech Support</h2>
+                    </div>
+                </div>
+                <div className="row row-content">
+                    <div className="col-8">
+                        <h5>Welcome to the tech support home page!</h5><br />
+                        <p> For a request for technical support, I invite you to fill out the form below. Expect a response within 24 hours during business hours (Monday through Friday 8am to 5pm) otherwise feel free to continue throughout the website! </p><br />
+                        <h5>Think You Can Solve it Yourself?</h5>< br/>
+                        <h5 className="offset-sm-3">Check Out Our Tutorials!</h5>
+                    </div>
+                    <div className="col"></div>
+                </div>
+                <div className="row row-content m-1">
+                    <div className="col-12">
+                        <h2 className="text-center">Support Request</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row row-content">
+                    <div className="col-md-10">
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup row>
+                                <Label htmlFor="firstName" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="firstName" name="firstName"
+                                        placeholder="First Name"
+                                        value={this.state.firstName}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastName" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastName" name="lastName"
+                                        placeholder="Last Name"
+                                        value={this.state.lastName}
+                                        onChange={this.handleInputChange} />
+                                </Col>                        
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="phoneNum" md={2}>Phone</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="phoneNum" name="phoneNum"
+                                        placeholder="Phone number"
+                                        value={this.state.phoneNum}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email"
+                                        placeholder="Email"
+                                        value={this.state.email}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 4, offset: 2}}>
+                                    <FormGroup>
+                                            <strong>How may we contact you?</strong>
+                                    </FormGroup>
+                                </Col>
+                                <Col md={4}>
+                                    <Input type="select" name="contactType"
+                                            value={this.state.contactType}
+                                            onChange={this.handleInputChange}>
+                                        <option>By Phone</option>
+                                        <option>By Email</option>
+                                        <option>By Text</option>
+                                    </Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="feedback" sm={12}>Please Describe Your Issue</Label>
+                                <Col sm={12}>
+                                    <Input type="textarea" id="feedback" name="feedback"
+                                        rows="12"
+                                        value={this.state.feedback}
+                                        onChange={this.handleInputChange}
+                                        placeholder="Click here to begin typing..."></Input>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col md={{size: 10}}>
+                                    <Button type="submit" color="primary">
+                                        Submit Request
+                                    </Button>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default Home;
