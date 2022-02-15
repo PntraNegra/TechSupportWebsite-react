625f1ba9e4391e21abc6129f1f60e9085b56ed69
Brian Cervantes <mr.cervantes@gmail.com>
2022-02-14 17:02:32 -0800

    embeding video tutrials

--- src/components/VideoTutorialsComponent.js
+++ src/components/VideoTutorialsComponent.js
@@ -47,56 +47,56 @@ function VideoTutorials(props) {
             <div id="wifi" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Connecting to wifi</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="Si2TbcE0r0U" />
                 </div>
             </div>
             <hr />
             <div id="update" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Updating Your Computer</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="7Uf4JQSpmq4" />
                 </div>
             </div>
             <hr />
             <div id="powerDown" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Powering Down Your Computer</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="vH4aeYIXsNA" />
                 </div>
             </div>
             <hr />
             <div id="openApps" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Opening Applications</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="8asJ0C20wWY" />
                 </div>
             </div>
             <hr />
             <div id="remoteDesktop" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Using Remote Desktop</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="SWRV9EXVyrk" />
                 </div>
             </div>
             <hr />
             <div id="zoom" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Joining a Zoom Meeting</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="SMbPgpjEmdw" />
                 </div>
             </div>
             <hr />
             <div id="email" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Sending an Email</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="j13ZU1ZF7Xo" />
                 </div>
             </div>
             <hr />
             <div id="socialMedia" className="row m-2">
                 <div className="col">
                     <h3 className="text-center mt-3 mb-3">Social Media</h3>
-                    <YoutubeEmbed embedId="TeNbP0v1wiM" />
+                    <YoutubeEmbed embedId="pcPR2V5oQ4M" />
                 </div>
             </div>
         </div>
